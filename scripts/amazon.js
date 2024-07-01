657b6d4e21b24549577417d5ad1c696bf61da21a
Benedict Tjandra <benedictbryan.tjandra@wsa.com>
2024-07-01 11:13:27 +0800

    make the art quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,6 +73,12 @@ document.querySelectorAll(".js-add-to-card-button").forEach((button, index) => {
         quantity:1
       })
     }
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    })
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    console.log(cartQuantity);
     console.log(cart);
   })
 })
