0553337c76073615e7a025ee0def273b5bfb3fb6
bryantjandra <tjandra.benedict@gmail.com>
2024-07-01 13:29:15 +0800

    move addToCart int cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from "../data/products.js";
 
 
@@ -58,31 +58,20 @@ products.forEach((product, index) => {
 });
 //Data attributes have to start with 'data-', its used for attaching any information to an element. 
 
+
+
+function updateCartQuantity() {
+  let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    })
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 document.querySelectorAll(".js-add-to-card-button").forEach((button, index) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
-    let matchingItem;
-    cart.forEach((item) => {
-      if(productId == item.productId) {
-        matchingItem = item;
-      }
-    })
-
-    if(matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity:1
-      })
-    }
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    })
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-    console.log(cartQuantity);
-    console.log(cart);
+    addToCart(productId);
+    updateCartQuantity();
   })
 })
