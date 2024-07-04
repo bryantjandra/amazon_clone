aae1e90b4ed572f94b46d01bdaebd6c9c6cca0a6
bryantjandra <tjandra.benedict@gmail.com>
2024-07-04 15:24:24 +0800

    load products from backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,11 @@
 import {cart, addToCart} from '../data/cart.js';
-import {products} from "../data/products.js";
+import {products, loadProducts} from "../data/products.js";
 import { formatCurrency } from './utils/money.js';
+
+loadProducts(renderProductsGrid);
+
+function renderProductsGrid() {
+
 
 
 let productsHTML = ``;
@@ -78,3 +83,5 @@ document.querySelectorAll(".js-add-to-card-button").forEach((button, index) => {
     updateCartQuantity();
   })
 })
+}
+
